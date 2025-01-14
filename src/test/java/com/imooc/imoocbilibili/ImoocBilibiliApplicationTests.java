e003c5873da91e9dfddcf06ddcbd9e2b0d2129ef
Gzr <1798414515@qq.com>
2025-01-13 16:07:21 -0800

    finish following and fan list

---
+++ src/test/java/com/imooc/imoocbilibili/ImoocBilibiliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.imoocbilibili;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImoocBilibiliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
